57127a15402aa5ed5c58513507c19748950c6279
fezin4472 <fernandolondero14@gmail.com>
2026-06-08 21:30:09 -0300

    add changes to ddn and botao

--- 06-ddn/script.js
+++ 06-ddn/script.js
@@ -5,3 +5,11 @@ function amor(){
 function removal(){
     document.getElementById('p-message').innerHTML = " "
 }
+
+function failbetter(){
+    document.getElementById('p-message').innerHTML = "Já tentou. Já falhou. Não importa. Tente de novo. Falhe de novo. Falhe melhor."
+}
+
+function bothmessages(){
+    document.getElementById('p-message').innerHTML = "eu te amo :) <br> Já tentou. Já falhou. Não importa. Tente de novo. Falhe de novo. Falhe melhor."
+}
