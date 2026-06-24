72206058c5553dea0084f62c0bef47f61a88331b
fezin4472 <fernandolondero14@gmail.com>
2026-06-23 22:13:39 -0300

    add new folder

---
+++ 09-sprint/script.js
@@ -0,0 +1,32 @@
+let i = 0
+
+function tarefas(){
+    let totalcon = 0
+    let totalncon = 0
+    let days = Number(prompt("quantos dia tem a sprint?"))
+    for(i = 1; i <= days; i++){
+        let fintasks = Number(prompt("quantas tarefas foram feitas no dia " + i + "?"))
+        let unfintasks = Number(prompt("quantas tarefas não foram feitas no dia " + i + "?"))
+        totalcon += fintasks
+        totalncon += unfintasks
+        document.getElementById('tekst').innerHTML += "DIA " + i + ": " + fintasks + " TAREFA(S) CONCLUIDA(S)" + " | " + unfintasks + " TAREFA(S) NAO CONCLUIDA(S)" + "<br>"
+        document.getElementById('tsket').innerHTML = "TOTAL DE TAREFAS CONCLUIDAS: " + totalcon + "<br>" + "TOTAL DE TAREFAS NAO CONCLUIDAS: " + totalncon
+    }
+}
+
+function notext(){
+    document.getElementById('tekst').innerHTML = " "
+    document.getElementById('tsket').innerHTML = " "
+}
+
+function bugs(){
+    let totalb = 0
+    let dias = Number(prompt("quantos dia tem a sprint?"))
+    for(i = 1; i <= dias; i++){
+        let bugs = Number(prompt("quantos bugs foram encontrado no dia " + i + "?"))
+        totalb += bugs
+        let media = totalb / dias
+        document.getElementById('tekst').innerHTML += "DIA " + i + ": " + bugs + "<br>"
+        document.getElementById('tsket').innerHTML = "TOTAL DE BUGS: " + totalb + "<br>" + "MEDIA DE BUGS: " + media
+    }
+}
