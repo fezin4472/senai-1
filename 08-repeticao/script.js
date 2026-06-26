da436755866c75dbc26d9c90955090ae26473f2a
fezin4472 <fernandolondero14@gmail.com>
2026-06-25 22:09:16 -0300

    add changes to repeticao and sprint

--- 08-repeticao/script.js
+++ 08-repeticao/script.js
@@ -4,10 +4,96 @@ function notext(){
 
 let i = 0
 
-function activities(){
-    let n = 5
+function exerciseone(){
+    let total = 0
+    for(i = 1; i <= 10; i++){
+        total += i
+        document.getElementById('text').innerHTML = total
+    }
+}
+
+function exercisetwo(){
+    for(i = 1; i <= 10; i++){
+        let five = i * 5
+        document.getElementById('text').innerHTML += five + "<br>"
+    }
+}
+
+function exercisethree(){
+    for(i = 1; i <= 10; i++){
+        let square = i * i
+        document.getElementById('text').innerHTML += square + "<br>"
+    }
+}
+
+function exercisefour(){
+    const javascript = ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
+    for(i = 0; i < javascript.length; i++){
+        document.getElementById('text').innerHTML += javascript[i] + "<br>"
+    }
+}
+
+function exercisefive(){
+    for(i = 1; i <= 10; i++){
+        let three = i * 3
+        document.getElementById('text').innerHTML += three + "<br>"
+    }
+}
+
+function exercisesix(){
+    const beep = "*"
+    for(i = 0; i < 10; i++){
+        document.getElementById('text').innerHTML += beep
+    }
+}
+
+function exerciseseven(){
+    let plus = 0
+    for(i = 2; i <= 20; i++){
+        let even = i * 2
+        plus = even + i
+        document.getElementById('text').innerHTML += plus + "<br>"
+    }
+}
+
+function exerciseeight(){
+    for(i = 0; i <= 10; i++){
+        let funf = i * 5
+        document.getElementById('text').innerHTML += funf + "<br>"
+    }
+}
+
+function exercisenine(){
+    const hello = "Olá"
+    for(i = 0; i < 8; i++){
+        document.getElementById('text').innerHTML += hello + "<br>"
+    }
+}
+
+function exerciseten(){
+    for(i = 0.5; i <= 7.5; i++){
+        let odd = i * 2
+        document.getElementById('text').innerHTML += odd + "<br>"
+    }
+}
+
+function exerciseeleven(){
+    const sequence = ["1", "12", "123", "1234", "12345"]
+    for(i = 0; i < sequence.length; i++){
+        document.getElementById('text').innerHTML += sequence[i] + "<br>"
+    }
+}
+
+function exercisetwelve(){
+    let factor = 5
     for(i = 4; i >= 1; i--){
-        n *= i
-        document.getElementById('text').innerHTML = n
+        let result = factor *= i
+        document.getElementById('text').innerHTML = result
+    }
+}
+
+function exercisethirteen(){
+    let word = String(prompt("digite uma palavra"))
+    for(i = 0; i < word.length; i++){
     }
 }
--- 09-sprint/script.js
+++ 09-sprint/script.js
@@ -17,16 +17,22 @@ function tarefas(){
 function notext(){
     document.getElementById('tekst').innerHTML = " "
     document.getElementById('tsket').innerHTML = " "
+    document.getElementById('texto').innerHTML = " "
+    document.getElementById('otxet').innerHTML = " "
 }
 
 function bugs(){
     let totalb = 0
     let dias = Number(prompt("quantos dia tem a sprint?"))
+    const array = []
     for(i = 1; i <= dias; i++){
-        let bugs = Number(prompt("quantos bugs foram encontrado no dia " + i + "?"))
+        let bugs = Number(prompt("quantos bugs foram encontrados no dia " + i + "?"))
         totalb += bugs
         let media = totalb / dias
+        array[i] = bugs
         document.getElementById('tekst').innerHTML += "DIA " + i + ": " + bugs + "<br>"
         document.getElementById('tsket').innerHTML = "TOTAL DE BUGS: " + totalb + "<br>" + "MEDIA DE BUGS: " + media
+        document.getElementById('texto').innerHTML = "MAIOR QUANTIDADE DE BUGS: " + bugs + "<br>" + "DIA COM MAIS BUGS: " + dias
+        document.getElementById('otxet').innerHTML = "MENOR QUANTIDADE DE BUGS: " + bugs + "<br>" + "DIA COM MENOS BUGS: " + dias
     }
 }
