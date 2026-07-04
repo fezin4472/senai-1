4cea8d9fe4a158c920ac02ab11bdbbce308153a6
fezin4472 <fernandolondero14@gmail.com>
2026-07-03 21:52:28 -0300

    add changes to arrays+ and repeticao

--- 08-repeticao/script.js
+++ 08-repeticao/script.js
@@ -5,95 +5,81 @@ function notext(){
 let i = 0
 
 function exerciseone(){
-    let total = 0
+    let square
     for(i = 1; i <= 10; i++){
+        square = i * i
+        document.getElementById('text').innerHTML += square + "<br>"
+    }
+}
+
+function exercisetwo(){
+    for(i = 10; i >= 1; i--){
+        document.getElementById('text').innerHTML += i + "<br>"
+    }
+}
+
+function exercisethree(){
+    let total
+    for(i = 1; i <= 100; i++){
         total += i
         document.getElementById('text').innerHTML = total
     }
 }
 
-function exercisetwo(){
+function exercisefour(){
+    let five
     for(i = 1; i <= 10; i++){
-        let five = i * 5
+        five = i * 5
         document.getElementById('text').innerHTML += five + "<br>"
     }
 }
 
-function exercisethree(){
+function exercisefourmod(){
+    let given = Number(prompt("bote um numero"))
+    let tabuada
     for(i = 1; i <= 10; i++){
-        let square = i * i
-        document.getElementById('text').innerHTML += square + "<br>"
-    }
-}
-
-function exercisefour(){
-    const javascript = ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
-    for(i = 0; i < javascript.length; i++){
-        document.getElementById('text').innerHTML += javascript[i] + "<br>"
-    }
-}
-
-function exercisefive(){
-    for(i = 1; i <= 10; i++){
-        let three = i * 3
-        document.getElementById('text').innerHTML += three + "<br>"
+        tabuada = i * given
+        document.getElementById('text').innerHTML += tabuada + "<br>"
     }
 }
 
 function exercisesix(){
-    const beep = "*"
-    for(i = 0; i < 10; i++){
-        document.getElementById('text').innerHTML += beep
+    let impar
+    for(i = 0.5; i < 10; i++){
+        impar = i * 2
+        document.getElementById('text').innerHTML += impar + "<br>"
     }
 }
 
 function exerciseseven(){
-    let plus = 0
-    for(i = 2; i <= 20; i++){
-        let even = i * 2
-        plus = even + i
-        document.getElementById('text').innerHTML += plus + "<br>"
-    }
+    for(i = 1; i <= 10; i++){}
 }
 
 function exerciseeight(){
-    for(i = 0; i <= 10; i++){
-        let funf = i * 5
-        document.getElementById('text').innerHTML += funf + "<br>"
+    let fatorial = Number(prompt("digite um numero"))
+    let resultado = 1
+    for(i = 1; i <= fatorial; i++){
+        resultado *= i
+        document.getElementById('text').innerHTML = resultado
     }
 }
 
 function exercisenine(){
-    const hello = "Olá"
-    for(i = 0; i < 8; i++){
-        document.getElementById('text').innerHTML += hello + "<br>"
+    for(i = 1; i <= 100; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            document.getElementById('text').innerHTML += "FizzBuzz"
+        }else if(i % 3 === 0){
+            document.getElementById('text').innerHTML += "Fizz"
+        }else if(i % 5 === 0){
+            document.getElementById('text').innerHTML += "Buzz"
+        }
+        document.getElementById('text').innerHTML += i + "<br>"
     }
 }
 
 function exerciseten(){
-    for(i = 0.5; i <= 7.5; i++){
-        let odd = i * 2
-        document.getElementById('text').innerHTML += odd + "<br>"
+    for(i = 1; i <= 10; i++){
     }
 }
 
-function exerciseeleven(){
-    const sequence = ["1", "12", "123", "1234", "12345"]
-    for(i = 0; i < sequence.length; i++){
-        document.getElementById('text').innerHTML += sequence[i] + "<br>"
-    }
-}
-
-function exercisetwelve(){
-    let factor = 5
-    for(i = 4; i >= 1; i--){
-        let result = factor *= i
-        document.getElementById('text').innerHTML = result
-    }
-}
-
-function exercisethirteen(){
-    let word = String(prompt("digite uma palavra"))
-    for(i = 0; i < word.length; i++){
-    }
-}
+function exerciseeleven(){}
--- 11-arrays+/script.js
+++ 11-arrays+/script.js
@@ -33,14 +33,15 @@ function addproduto(){
 }
 
 let exercicioi = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado', 'domingo'];
+const exercicioii = ['domingo', 'sábado', 'sexta', 'quinta', 'quarta', 'terça', 'segunda']
 const exercicioiii = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado', 'domingo', 'feriado'];
 const exercicioiv = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'];
 let exerciciov = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 const exerciciovi = [2, 4, 12, 8, 10, 12, 14, 16, 18, 20];
 let exerciciovii = ['banana', 'maçã', 'uva'];
 const exercicioviii = ['banana', 'uva', 'morango'];
-const exercicioix = ['jeff', 'jeni', 'ph'];
-const exerciciox = ['jeff', 'jeni', 'ph', 'polacos', 'olavo', 'conte'];
+const exercicioix = ['jeferson', 'jeni', 'ph'];
+const exerciciox = ['jeferson', 'jeni', 'ph', 'polacos', 'olavo', 'conte'];
 const exercicioxi = [33, 19, 15, 18, 18, 18];
 let exercicioxii = 52
 let exercicioxiii = 20.1666
@@ -49,8 +50,33 @@ const exercicioxv = ['cacetinho', 'powerade', 'arroz', 'pasta de dente', 'desodo
 const exercicioxvi = ['cacetinho', 'powerade', 'pasta de dente', 'desodorante'];
 const exercicioxvii = false
 const exercicioxviii = [3, 15, 29, 45, 31];
-const exercicioxix = [6, 30, 58, 90, 62];
-const exercicioxx = ['gramado', 'oslo', 'wolfsburg', 'tóquio', 'florianópolis'];
+let impares = [];
+const exercicioxix = [180, 4, 90, 69, ];
+let numeros = [90, 2, 45]
+const exercicioxx = ['gramado', 'oslo', 'wolfsburg', 'nagoia', 'florianópolis', 'hell'];
+
+function removetext(){
+    document.getElementById('textoi').innerHTML = ""
+    document.getElementById('textoii').innerHTML = ""
+    document.getElementById('textoiii').innerHTML = ""
+    document.getElementById('textoiv').innerHTML = ""
+    document.getElementById('textov').innerHTML = ""
+    document.getElementById('textovi').innerHTML = ""
+    document.getElementById('textovii').innerHTML = ""
+    document.getElementById('textoviii').innerHTML = ""
+    document.getElementById('textoix').innerHTML = ""
+    document.getElementById('textox').innerHTML = ""
+    document.getElementById('textoxi').innerHTML = ""
+    document.getElementById('textoxii').innerHTML = ""
+    document.getElementById('textoxiii').innerHTML = ""
+    document.getElementById('textoxiv').innerHTML = ""
+    document.getElementById('textoxv').innerHTML = ""
+    document.getElementById('textoxvi').innerHTML = ""
+    document.getElementById('textoxvii').innerHTML = ""
+    document.getElementById('textoxviii').innerHTML = ""
+    document.getElementById('textoxix').innerHTML = ""
+    document.getElementById('textoxx').innerHTML = ""
+}
 
 function funcoes(){
 
@@ -152,25 +178,25 @@ function funcoes(){
 
     document.getElementById('textoxvii').innerHTML = check
 
+    impares.push(15,)
     for(i = 0; i < exercicioxviii.length; i++){
         document.getElementById('textoxviii').innerHTML += exercicioxviii[i] + "<br>"
     }
 
-    let multiplyone = exercicioxviii[0] * 2
-    let multiplytwo = exercicioxviii[1] * 2
-    let multiplythree = exercicioxviii[2] * 2
-    let multiplyfour = exercicioxviii[3] * 2
-    let multiplyfive = exercicioxviii[4] * 2
+    let multiplier
 
     for(i = 0; i < exercicioxviii.length; i++){
-        document.getElementById('textoxix').innerHTML += exercicioxviii[i] + "<br>"
+        multiplier = exercicioxviii[i] * 2
+        document.getElementById('textoxix').innerHTML += multiplier + "<br>"
     }
 
     document.getElementById('textoxx').innerHTML = "Eu não sei porque, mas eu gosto de frio, e cidades que podem ficar frias (especialmente no inverno) são " + exercicioxx[0] + 
     " no estado do rio grande do sul (adoro essa cidade) e " + exercicioxx[1] + " na noruega. Se eu poderia morar em um outro lugar além de " + 
     exercicioxx[4] + " e " + exercicioxx[0] + 
-    " seria esse, porque eu ouvi dizer que quando você sai, é calmo e quieto, e também tem o sol da meia noite e as luzes do norte (também chamado de aurora boreal) e que eu gosto do frio. E também estou aprendendo a falar alemão, eu gostaria de ir para a alemanha e visitar " + 
+    " seria esse, porque eu ouvi dizer que quando você sai, é calmo e quieto, e também tem o sol da meia noite e as luzes do norte (também chamado de aurora boreal) e que eu gosto do frio. Tem também uma cidade chamado de " + 
+    exercicioxx[5] + " que é bem longe de " + exercicioxx[1] + ". Tem até um circuito de rallycross chamado de 'Lånkebanen' que sedia o campeonato Europeu de rallycross desde 2011 e o mundial de rallycross de 2014 a 2025 quando o campeonato foi substituído com o europeu (um dia eu vou para " 
+    + exercicioxx[5] + " e ver uma corrida de rallycross confia em mim eu vou ser top 50 no beach e fazer isso hhehehehehhehehe). E também estou aprendendo a falar alemão, eu gostaria de ir para a alemanha e visitar " + 
     exercicioxx[2] + " que é onde a fabrica da volkswagen na alemanha é, e também gostaria ir para " + 
-    exercicioxx[3] + " no japão porque eu gosto de carros, e japão e a alemanha são os paises que tem as minhas marcas favoritas"
+    exercicioxx[3] + " no japão (capital da província de aichi), 25 kilometros nordeste da fabrica da toyota."
 }
 
