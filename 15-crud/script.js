cc563651522790511d6c648db1899a6f539479e5
fezin4472 <fernandolondero14@gmail.com>
2026-07-17 21:56:38 -0300

    rename 4 files

--- 15-crud/script.js
+++ 15-crud/script.js
@@ -31,10 +31,33 @@ function clearform(){
     document.getElementById('input-cor').value = ""
     document.getElementById('input-ano').value = ""
     document.getElementById('input-fipe').value = ""
+    document.getElementById('input-id').value = ""
 }
 
 function show(){
     for(i = 0; i < carros.length; i++){
         document.getElementById('panel').innerHTML += `<p class="card-carros">${carros[i].carro}</p>`
     }
+}
+
+function search(){
+    let procurado = document
+}
+
+function save(){
+    let id = Number(document.getElementById('input-id').value)
+
+    for(i = 0; i < carros.length; i++){
+        if(id == carros[i].id){
+            console.log(carros[i]);
+            carros[i].potencia = document.getElementById('input-potencia').value
+            carros[i].cor = document.getElementById('input-cor').value
+            carros[i].ano = document.getElementById('input-ano').value
+            carros[i].fipe = document.getElementById('input-fipe').value
+            carros[i].id = document.getElementById('input-id').value
+            console.log(i);
+        }
+    }
+    show();
+    clearform();
 }
